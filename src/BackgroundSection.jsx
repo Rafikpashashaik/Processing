10cdf0859cfbe2f81ae7888841573d3e816f4658
Rafi shaik <135604633+Rafikpashashaik@users.noreply.github.com>
2023-09-28 14:22:36 +0530

    Add files via upload

---
+++ src/BackgroundSection.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./BackgroundSection.css"; // Import your CSS file for styling this component
+import taxiImage from "./Images/pngwing.com.png"; // Provide the correct path to your image
+import { Slide } from "react-awesome-reveal";
+
+function BackgroundSection() {
+  return (
+    <section className="background-section">
+      <div className="container">
+        {/* Your content for this section goes here */}
+        <Slide direction="left" triggerOnce>
+          <h1 className="hcolor">
+            RIDE<span className="tcolor">RE</span>
+          </h1>
+          <h3 className="pcolor">WAY TO RIDE SPACE TO PARK</h3>
+        </Slide>
+      </div>
+      <Slide direction="right" triggerOnce>
+        <div>
+          <img src={taxiImage} alt="Taxi Logo" className="backgroundimage" />
+        </div>
+      </Slide>
+    </section>
+  );
+}
+
+export default BackgroundSection;
